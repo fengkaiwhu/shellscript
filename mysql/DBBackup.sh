1ae8b3a61acd77de4d15fbf960761a825eb9affb
fengkaiwhu <kaifeng@whu.edu.cn>
2015-12-24 15:45:42 +0800

    fisrt commit

--- mysql/DBBackup.sh
+++ mysql/DBBackup.sh
@@ -15,18 +15,27 @@ BCK_DIR="/tmp/dbbackup"
 DATE=`date +%F`
 
 # TODO
-mkdir -p $BCK_DIR/$DATE/ &> /dev/null || { echo "mkdir \"$BCK_DIR/$DATE/\" failed!"; exit 1; }
 
 type mysqldump &> /dev/null || { echo "mysqldump is not installed!";exit 1; }
 type gzip &> /dev/null || { echo "gzip is not installed!";exit 1; }
 
-mysql -u$LDB_USER -p$LDB_PASS -e "" &> /dev/null || { echo "Bad mysql username or passwd! Exit!"; exit 1; }
+# Checking infomation
+echo -n "Checking Local Mysql info......"
+mysql -u$LDB_USER -p$LDB_PASS -e "" &> /dev/null && echo "Pass" || { echo "Bad mysql username or passwd! Exit!"; exit 1; }
+
+for database in $DB
+do
+    echo -n "Checking DB \"$database\"......"
+    mysql -u$LDB_USER -p$LDB_PASS $database -e "" &> /dev/null && echo "Pass" || { echo "Not Exists";exit 1; }
+done
+
+echo -n "Creating Backup dir......"
+mkdir -p $BCK_DIR/$DATE/ &> /dev/null && echo "Pass" || { echo "mkdir \"$BCK_DIR/$DATE/\" failed!"; exit 1; }
 
 for database in $DB
 do
     echo "***************************************"
     echo "DB \"$database\" being backed up......"
-    mysql -u$LDB_USER -p$LDB_PASS $database -e "" &> /dev/null || { echo "\"$database\" is not exists! Skip it!"; continue; }
     # --no-data 只导出数据表结构而不导出数据
     mysqldump --opt --force --log-error=error.log \
         -u$LDB_USER -p$LDB_PASS $database | \
--- mysql/DBMigration.sh
+++ mysql/DBMigration.sh
@@ -11,16 +11,33 @@ LDB_PASS="123456"
 DB="enNercms db2"
 
 # Remote Database info
-RDB_USER="root"
-RDB_PASS="y1nghu@rkai"
+RDB_USER="rot"
+RDB_PASS="y1ai"
 
 # Remote Server info
-user="bbs"
-ip="202.114.96.30"
-pass='logosun!@#$#' #建议提前设置ssh无密码登录
-port="8000"
+user="bs"
+ip="20.14.6.0"
+pass='lo#$#' #建议提前设置ssh无密码登录
+port="8"
 
+# TODO
 type mysqldump &> /dev/null || { echo "mysqldump is not installed!";exit 1; }
+type ssh &> /dev/null || { echo "mysqldump is not installed!";exit 1; }
+
+# Checking infomation
+echo -n "Checking Local Mysql info......"
+mysql -u$LDB_USER -p$LDB_PASS -e "" &> /dev/null && echo "Pass" || { echo "Bad Local mysql username or passwd! Exit!"; exit 1; }
+
+for database in $DB
+do
+    echo -n "Checking DB \"$database\"......"
+    mysql -u$LDB_USER -p$LDB_PASS $database -e "" &> /dev/null && echo "Pass" || { echo "Not Exists";exit 1; }
+done
+
+echo -n "Checking Remote Server info......"
+ssh -p $port $user@$ip date &> /dev/null && echo "Pass" || { echo "Bad Remote Server info! EXit!";exit 1; }
+echo -n "Checking Remote Mysql info......"
+ssh -p $port $user@$ip "mysql -u$RDB_USER -p$RDB_PASS -e ' '" &> /dev/null && echo "Pass" || { echo "Bad Remote mysql username or passwd! EXit!";exit 1; }
 
 for database in $DB
 do
@@ -30,7 +47,7 @@ do
     # echo "create database $database" | ssh -C -p $port $user@$ip "mysql -u$RDB_USER -p$RDB_PASS"
     echo "drop database if exists $database; create database $database;" | ssh -C -p $port $user@$ip "mysql -u$RDB_USER -p$RDB_PASS" 
 
-    # 接着创建相关的表和数据
+    # Migrating from local to remote 
     echo "Migrating DB \"$database\"......"
     # --no-data 只导出数据表结构而不导出数据
     mysqldump --extended-insert --add-drop-table --add-drop-database --lock-tables --force --log-error=error.log \
